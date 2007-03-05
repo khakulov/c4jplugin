5f8a36c12338853d0ba7882c11376fc1c203a716
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2007-03-05 09:08:31 +0000

    Refactored contract hierarchy to support tree and graph (zest) based visualizations

--- net.sourceforge.c4jplugin/trunk/src/net/sourceforge/c4jplugin/internal/ui/contracthierarchy/ContractHierarchyContentProvider.java
+++ net.sourceforge.c4jplugin/trunk/src/net/sourceforge/c4jplugin/internal/ui/contracthierarchy/ContractHierarchyContentProvider.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 package net.sourceforge.c4jplugin.internal.ui.contracthierarchy;
 
 import java.util.ArrayList;
@@ -20,7 +10,7 @@ import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.ui.IWorkingCopyProvider;
-import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
@@ -30,7 +20,7 @@ import org.eclipse.jface.viewers.ViewerFilter;
  * Implementors must override 'getContractsInHierarchy'.
  * Java delta processing is also performed by the content provider
  */
-public abstract class ContractHierarchyContentProvider implements ITreeContentProvider, IWorkingCopyProvider {
+public abstract class ContractHierarchyContentProvider implements IStructuredContentProvider, IWorkingCopyProvider {
 	protected static final Object[] NO_ELEMENTS= new Object[0];
 	
 	protected ContractHierarchyLifeCycle fTypeHierarchy;
@@ -81,18 +71,7 @@ public abstract class ContractHierarchyContentProvider implements ITreeContentPr
 		return filterOverrides;
 	}
 	
-	private void addCompatibleMethods(IMethod filterMethod, IType typeToFindIn, List children) throws JavaModelException {
-		boolean filterMethodOverrides= initializeConditionOverrideTester(filterMethod, typeToFindIn);
-		IMethod[] methods= typeToFindIn.getMethods();
-		for (int i= 0; i < methods.length; i++) {
-			IMethod curr= methods[i];
-			if (isCompatibleCondition(filterMethod, curr, filterMethodOverrides) && !children.contains(curr)) {
-				children.add(curr);
-			}
-		}
-	}
-	
-	private boolean hasCompatibleCondition(IMethod filterMethod, IType typeToFindIn) throws JavaModelException {
+	protected boolean hasCompatibleCondition(IMethod filterMethod, IType typeToFindIn) throws JavaModelException {
 		boolean filterMethodOverrides= initializeConditionOverrideTester(filterMethod, typeToFindIn);
 		IMethod[] methods= typeToFindIn.getMethods();
 		for (int i= 0; i < methods.length; i++) {
@@ -150,7 +129,7 @@ public abstract class ContractHierarchyContentProvider implements ITreeContentPr
 		for (int i= types.size() - 1; i >= 0; i--) {
 			IType curr= (IType) types.get(i);
 			try {
-				if (!isInTree(curr)) {
+				if (!isInControl(curr)) {
 					types.remove(i);
 				}
 			} catch (JavaModelException e) {
@@ -171,22 +150,23 @@ public abstract class ContractHierarchyContentProvider implements ITreeContentPr
 		}
 	}
 	
+	protected void addCompatibleMethods(IMethod filterMethod, IType typeToFindIn, List children) throws JavaModelException {
+		boolean filterMethodOverrides= initializeConditionOverrideTester(filterMethod, typeToFindIn);
+		IMethod[] methods= typeToFindIn.getMethods();
+		for (int i= 0; i < methods.length; i++) {
+			IMethod curr= methods[i];
+			if (isCompatibleCondition(filterMethod, curr, filterMethodOverrides) && !children.contains(curr)) {
+				children.add(curr);
+			}
+		}
+	}
+	
 	/**
 	 * Hook to overwrite. Filter will be applied on the returned types
 	 */	
 	protected abstract void getContractsInHierarchy(IType type, List<IType> res);
 	
-	/**
-	 * Hook to overwrite. Return null if parent is ambiguous.
-	 */	
-	protected IType getParentContract(IType type) {
-		IContractHierarchy hierarchy= getHierarchy();
-		if (hierarchy != null) {
-			IType[] supers = hierarchy.getSupercontracts(type);
-			if (supers != null && supers.length == 1) return supers[0];
-		}
-		return null;
-	}
+	
 	
 	
 	private boolean isInScope(IType type) {
@@ -211,69 +191,7 @@ public abstract class ContractHierarchyContentProvider implements ITreeContentPr
 		return false;
 	}
 	
-	/*
-	 * Called for the tree children.
-	 * @see ITreeContentProvider#getChildren
-	 */	
-	public Object[] getChildren(Object element) {
-		if (element instanceof IType) {
-			try {
-				IType type= (IType)element;
-	
-				List<IMember> children= new ArrayList<IMember>();
-				if (fMemberFilter != null) {
-					addFilteredMemberChildren(type, children);
-				}
-	
-				addTypeChildren(type, children);
-				
-				return children.toArray();
-			} catch (JavaModelException e) {
-				// ignore
-			}
-		}
-		return NO_ELEMENTS;
-	}
-	
-	/*
-	 * @see ITreeContentProvider#hasChildren
-	 */
-	public boolean hasChildren(Object element) {
-		if (element instanceof IType) {
-			try {
-				IType type= (IType) element;
-				return hasTypeChildren(type) || (fMemberFilter != null && hasMemberFilterChildren(type));
-			} catch (JavaModelException e) {
-				return false;
-			}			
-		}
-		return false;
-	}	
-	
-	private void addFilteredMemberChildren(IType parent, List<IMember> children) throws JavaModelException {
-		for (int i= 0; i < fMemberFilter.length; i++) {
-			IMember member= fMemberFilter[i];
-			if (parent.equals(member.getDeclaringType())) {
-				if (!children.contains(member)) {
-					children.add(member);
-				}
-			} else if (member instanceof IMethod) {
-				addCompatibleMethods((IMethod) member, parent, children);
-			}
-		}		
-	}
-		
-	private void addTypeChildren(IType type, List<IMember> children) throws JavaModelException {
-		ArrayList<IType> types= new ArrayList<IType>();
-		getContractsInHierarchy(type, types);
-		for (IType curr : types) {
-			if (isInTree(curr)) {
-				children.add(curr);
-			}
-		}
-	}
-	
-	protected final boolean isInTree(IType type) throws JavaModelException {
+	protected final boolean isInControl(IType type) throws JavaModelException {
 		if (isInScope(type)) {
 			if (fMemberFilter != null) {
 				return hasMemberFilterChildren(type) || hasTypeChildren(type);
@@ -302,7 +220,7 @@ public abstract class ContractHierarchyContentProvider implements ITreeContentPr
 		ArrayList<IType> types= new ArrayList<IType>();
 		getContractsInHierarchy(type, types);
 		for (IType curr : types) {
-			if (isInTree(curr)) {
+			if (isInControl(curr)) {
 				return true;
 			}
 		}
@@ -325,20 +243,6 @@ public abstract class ContractHierarchyContentProvider implements ITreeContentPr
 		
 	}
 	
-	/*
-	 * @see ITreeContentProvider#getParent
-	 */
-	public Object getParent(Object element) {
-		if (element instanceof IMember) {
-			IMember member= (IMember) element;
-			if (member.getElementType() == IJavaElement.TYPE) {
-				return getParentContract((IType)member);
-			}
-			return member.getDeclaringType();
-		}
-		return null;
-	}
-	
 	protected final boolean isAnonymous(IType type) {
 		return type.getElementName().length() == 0;
 	}
@@ -352,9 +256,5 @@ public abstract class ContractHierarchyContentProvider implements ITreeContentPr
 	protected final boolean isObject(IType type) {
 		return "Object".equals(type.getElementName()) && type.getDeclaringType() == null && "java.lang".equals(type.getPackageFragment().getElementName());  //$NON-NLS-1$//$NON-NLS-2$
 	}
-
-
-
-
 	
 }
